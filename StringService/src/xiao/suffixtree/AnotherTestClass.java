c6e15666c8d7b5c21a626d2f0718c5f0ba2c6b21
Xiao <Xiao@Xiao-PC>
2013-05-31 17:49:44 -0400

    Test commit from desktop

---
+++ StringService/src/xiao/suffixtree/AnotherTestClass.java
@@ -0,0 +1,13 @@
+package xiao.suffixtree;
+
+public class AnotherTestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
